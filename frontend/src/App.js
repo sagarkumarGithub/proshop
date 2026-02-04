193cbc560866663cd43f47a04b31aff1855d9638
Sagar Kumar <sagarkumar15839@gmail.com>
2026-02-04 14:34:33 +0530

    created product listing and product details page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,19 @@
-import React from 'react'
+import Header from './components/Header'
+import { Outlet } from 'react-router-dom'
+import { Container } from 'react-bootstrap'
+import Footer from './components/Footer'
 
 const App = () => {
   return (
-    <div>Welcome to Proshop</div>
+    <>
+      <Header />
+      <main className='py-3'>
+        <Container>
+          <Outlet />
+        </Container>
+      </main>
+      <Footer />
+    </>
   )
 }
 
