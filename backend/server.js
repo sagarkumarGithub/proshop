2052e8333199e4c452cb7299df3cbe7d7e478a67
Sagar Kumar <sagarkumar15839@gmail.com>
2026-02-16 22:29:48 +0530

    ready for deployment

--- backend/server.js
+++ backend/server.js
@@ -21,10 +21,6 @@ app.use(express.urlencoded({ extended: true }));
 // Cookie parser middleware
 app.use(cookieParser());
 
-app.get("/", (req, res) => {
-    res.send("API is running");
-});
-
 app.use("/api/products", productRoutes); 
 app.use("/api/users", userRoutes);
 app.use("/api/orders", orderRoutes);
@@ -36,6 +32,21 @@ app.get("/api/config/paypal", (req, res) => {
 
 const __dirname = path.resolve();
 app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
+
+if (process.env.NODE_ENV === "production") {
+
+    // Set static folder
+    app.use(express.static(path.join(__dirname, "/frontend/build")));
+
+    // any route that doesn't start with /api will go to index.html
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
+    });
+}else{
+    app.get("/", (req, res) => {
+        res.send("API is running");
+    });
+}
 
 app.use(notFound);
 app.use(errorHandler);
